b835bd849f3e62d0632737e990e83dcbfb3d86fe
Bui Van Thai <buithaild230899@gmail.com>
2023-02-27 23:35:26 +0700

    update

--- src/day3b/SinhVien/Main.java
+++ src/day3b/SinhVien/Main.java
@@ -1,4 +1,14 @@
 package day3b.SinhVien;
 
 public class Main {
+    public static void main(String[] args) {
+        SinhVien s1 = new SinhVien();
+        s1.setLT(7);
+        s1.setTH(8);
+        System.out.println(s1);
+        System.out.println("Diem trung binh: " + s1.diemTB());
+
+        //-------------
+        SinhVien s2 = new SinhVien();
+    }
 }
--- src/day3b/SinhVien/SinhVien.java
+++ src/day3b/SinhVien/SinhVien.java
@@ -1,5 +1,64 @@
 package day3b.SinhVien;
 
 public class SinhVien {
+    private  int id;
+    private String hoTen;
+    private float LT;
+    private float TH;
+    public  SinhVien(){
 
+    }
+
+    public SinhVien(int id, String hoTen, float LT, float TH) {
+        this.id = id;
+        this.hoTen = hoTen;
+        this.LT = LT;
+        this.TH = TH;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public float getLT() {
+        return LT;
+    }
+
+    public void setLT(float LT) {
+        this.LT = LT;
+    }
+
+    public float getTH() {
+        return TH;
+    }
+
+    public void setTH(float TH) {
+        this.TH = TH;
+    }
+
+    @Override
+    public String toString() {
+        return "SinhVien{" +
+                "id=" + id +
+                ", hoTen='" + hoTen + '\'' +
+                ", LT=" + LT +
+                ", TH=" + TH +
+                '}';
+    }
+
+    public double diemTB(){
+        return (LT+TH)/2;
+    }
 }
